2a5c28e5277025768e64438ba24ce1446e2a8f3b
Levin Uncu <levin@uncu.de>
2024-04-08 14:07:56 +0200

    chore: removed bump from commitlint type-enum

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -14,7 +14,7 @@ const config: UserConfig = {
     'type-enum': [
       RuleConfigSeverity.Error,
       'always',
-      ['feat', 'fix', 'refactor', 'test', 'build', 'ci', 'bump'],
+      ['feat', 'fix', 'refactor', 'test', 'build', 'ci'],
     ],
   },
 };
