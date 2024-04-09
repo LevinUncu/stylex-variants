0cd92abc8f6640d1d282314823e2efab2d26e34b
Levin Uncu <levin@uncu.de>
2024-04-10 00:09:50 +0200

    refactor(library): improved types

--- library/src/types.ts
+++ library/src/types.ts
@@ -1,23 +1,54 @@
 import { StyleXStyles } from '@stylexjs/stylex';
 
-type Variants = Record<string, Record<string, StyleXStyles>>;
+type StyleVariant = StyleXStyles | boolean | undefined;
 
-export type Options<T> = T extends Variants
-  ? {
-      base?: StyleXStyles;
-      variants?: T;
-      defaultVariants?: AvailableVariants<T>;
-    }
-  : {
-      base?: StyleXStyles;
-      variants?: never;
-      defaultVariants?: never;
-    };
+interface Variant {
+  [key: string]: StyleVariant;
+  required?: boolean;
+}
+
+export interface Variants {
+  [key: string]: Variant;
+}
+
+interface Base {
+  base?: StyleXStyles;
+}
 
 type StringToBoolean<T> = T extends 'true' | 'false' ? boolean : T;
 
-export type AvailableVariants<T> = T extends Variants
-  ? {
-      [key in keyof T]?: StringToBoolean<keyof T[key]>;
-    }
-  : never;
+type VariantKeys<T> = keyof T;
+type VariantValues<T> = StringToBoolean<
+  keyof Omit<T[VariantKeys<T>], 'required'>
+>;
+
+interface VariantOptions<T extends Variants> {
+  variants?: T;
+  defaultVariants?: Partial<Record<VariantKeys<T>, VariantValues<T>>>;
+}
+
+export type Options<T extends Variants> = Base & VariantOptions<T>;
+
+type RequiredVariants<T> = {
+  [K in keyof T]: T[K] extends { required: true } ? K : never;
+}[keyof T];
+
+type OptionalVariants<T> = {
+  [K in keyof T]: T[K] extends { required: true } ? never : K;
+}[keyof T];
+
+type HasRequired<T> =
+  RequiredVariants<T> extends Record<string, never> ? false : true;
+
+type RequiredVariantKeys<T> = {
+  [K in RequiredVariants<T>]: VariantValues<T>;
+};
+
+type OptionalVariantKeys<T> = {
+  [K in OptionalVariants<T>]?: VariantValues<T>;
+};
+
+export type SelectedVariants<T extends Variants> =
+  HasRequired<T> extends true
+    ? RequiredVariantKeys<T> & OptionalVariantKeys<T>
+    : OptionalVariantKeys<T> | undefined;
