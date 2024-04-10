5a7ac48c53d870c1a06daf59d555391811c82a1c
Levin Uncu <levin@uncu.de>
2024-04-10 10:51:15 +0200

    feat(library): required variants

--- library/src/index.ts
+++ library/src/index.ts
@@ -1,33 +1,39 @@
 import { type StyleXStyles } from '@stylexjs/stylex';
-import { Options, AvailableVariants } from './types';
+import { Options, Variants, SelectedVariants } from './types';
 
-export type VariantProps<
-  T extends (selectedVariants?: AvailableVariants<T>) => StyleXStyles[],
-> = Parameters<T>[0];
+export type VariantProps<T extends (selectedVariants: any) => StyleXStyles[]> =
+  Parameters<T>[0];
 
-export function sv<T>(options: Options<T>) {
-  return function (selectedVariants?: AvailableVariants<T>): StyleXStyles[] {
-    const base = options?.base;
-    if (!options?.variants) {
+export function sv<T extends Variants>(options: Options<T>) {
+  return function (selectedVariants: SelectedVariants<T>): StyleXStyles[] {
+    const { base, variants, defaultVariants } = options;
+
+    if (!variants) {
       return base ? [base] : [];
     }
-    const { variants, defaultVariants } = options;
+
     const variantClassnames: StyleXStyles[] = [];
 
     for (const variant in variants) {
-      const selectedVariant = selectedVariants?.[
-        variant
-      ] as keyof (typeof variants)[typeof variant];
-      const defaultVariant = defaultVariants?.[
-        variant
-      ] as keyof (typeof variants)[typeof variant];
+      const selectedVariant = selectedVariants?.[variant] as string | undefined;
+      const defaultVariant = defaultVariants?.[variant] as string | undefined;
 
-      if (selectedVariant) {
-        variantClassnames.push(variants[variant][selectedVariant]);
-      } else if (defaultVariant) {
-        variantClassnames.push(variants[variant][defaultVariant]);
+      if (variants[variant].required && !selectedVariant) {
+        console.error(
+          `variant ${String(variant)} is required but not selected.`
+        );
+      }
+
+      if (selectedVariant && variants[variant][selectedVariant]) {
+        variantClassnames.push(
+          variants[variant][selectedVariant] as StyleXStyles
+        );
+      } else if (defaultVariant && variants[variant][defaultVariant]) {
+        variantClassnames.push(
+          variants[variant][defaultVariant] as StyleXStyles
+        );
       } else if (variants[variant].false) {
-        variantClassnames.push(variants[variant].false);
+        variantClassnames.push(variants[variant].false as StyleXStyles);
       }
     }
 
